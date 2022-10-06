74ffd3d0cd17e8e33f35b857f4d784434df94e67
calmgiraffe <hans.park6@gmail.com>
2022-10-06 18:17:12 -0400

    more basic load screen functions

--- proj2/byog/Core/Game.java
+++ proj2/byog/Core/Game.java
@@ -10,67 +10,93 @@ import java.awt.Font;
 public class Game {
     private static final int WIDTH = 60;
     private static final int HEIGHT = 40;
+    private static final Font title = new Font("Consolas", Font.BOLD, 40);
+    private static final Font option = new Font("Consolas", Font.PLAIN, 28);
+    private static final Font tileFont = new Font("Monaco", Font.BOLD, 14);
     private final TERenderer ter = new TERenderer();
     private Map map;
 
     /**
      * Method used for playing a fresh game. The game should start from the main menu.
+     * Loop until proper user input is obtained, then go to appropriate method.
      */
     public void playWithKeyboard() {
         ter.initialize(WIDTH, HEIGHT);
 
-        // Display loading screen text; initial text options in first block of code
-        Font title = new Font("Consolas", Font.BOLD, 40);
-        Font option = new Font("Consolas", Font.PLAIN, 30);
-        StdDraw.setPenColor(Color.WHITE);
-
-        StdDraw.setFont(title);
-        StdDraw.text(30 * WIDTH / 60.0, 30 * HEIGHT / 40.0, "Roguelike Dungeon Game");
-
-        StdDraw.setFont(option);
-        StdDraw.text(30 * WIDTH / 60.0, 22 * HEIGHT / 40.0, "New Game (N)");
-        StdDraw.text(30 * WIDTH / 60.0, 20 * HEIGHT / 40.0, "Load Game (L)");
-        StdDraw.text(30 * WIDTH / 60.0, 18 * HEIGHT / 40.0, "Quit (Q)");
-
-        StdDraw.show();
-
-        // wait for user input
-        // if input == 'n', go to input seed screen
-        label:
         while (true) {
-            String input = getUserInput(1);
+            StdDraw.clear(Color.BLACK);
+            StdDraw.setPenColor(Color.WHITE);
+            StdDraw.setFont(title);
+            StdDraw.text(30 * WIDTH / 60.0, 26 * HEIGHT / 40.0, "CS61B: The Game");
+            StdDraw.setFont(option);
+            StdDraw.text(30 * WIDTH / 60.0, 18 * HEIGHT / 40.0, "New Game (N)");
+            StdDraw.text(30 * WIDTH / 60.0, 16 * HEIGHT / 40.0, "Load Game (L)");
+            StdDraw.text(30 * WIDTH / 60.0, 14 * HEIGHT / 40.0, "Quit (Q)");
+            StdDraw.show();
+
+            char input = getUserChar();
             switch (input) {
-                case "n":
-                    System.out.println("n was typed");
-                    break label;
-                case "l":
-                    System.out.println("l was typed");
-                    break label;
-                case "q":
-                    System.out.println("q was typed");
-                    break label;
+                case 'n' -> inputSeedScreen();
+                case 'l' -> loadGame();
+                case 'q' -> System.exit(0);
             }
         }
-        System.exit(0);
     }
 
     /**
-     * Waits for the user to type in a String of length n. Once the string in the queue
-     * reaches length n, the method returns the string.
-     * @param n length of string that is desired from the user
+     * Loops infinitely, reading user input and appending each character to variable "input".
+     * Once the string's length reaches length n, the loop ends, and the method returns the string.
      * @return resulting string typed by user
      */
-    private String getUserInput(int n) {
-        StringBuilder input = new StringBuilder();
-        while (input.length() < n) {
-            if (StdDraw.hasNextKeyTyped()) {
-                char key = StdDraw.nextKeyTyped();
-                input.append(key);
+    private char getUserChar() {
+        while (true) {
+            if (!StdDraw.hasNextKeyTyped()) {
+                continue;
             }
+            return StdDraw.nextKeyTyped();
         }
-        return input.toString();
     }
 
+    /**
+     * Displays the seed input screen; backspace to return to main menu,
+     * s to submit the seed and generate a new map.
+     */
+    private void inputSeedScreen() {
+        StdDraw.clear(Color.BLACK);
+
+        StringBuilder seed = new StringBuilder();
+        boolean seedTyped = false;
+        while (!seedTyped) {
+            StdDraw.setFont(title);
+            StdDraw.text(30 * WIDTH / 60.0, 26 * HEIGHT / 40.0, "CS61B: The Game");
+            StdDraw.setFont(option);
+            StdDraw.text(30 * WIDTH / 60.0, 18 * HEIGHT / 40.0, "(s to submit, Esc to return)");
+            StdDraw.text(30 * WIDTH / 60.0, 14 * HEIGHT / 40.0, "Seed: " + seed);
+            StdDraw.show();
+
+            char c = getUserChar();
+            if ((int) c == 8) { // ASCII backspace
+                if (seed.length() > 0) {
+                    seed.deleteCharAt(seed.length() - 1);
+                }
+            } else if (c == 's' && seed.length() > 0) {
+                seedTyped = true;
+            } else if ((int) c == 27) { // ASCII esc key
+                return;
+            } else if (seed.length() < 15) { // Max seed length is 16
+                seed.append(c);
+            }
+            StdDraw.clear(Color.BLACK);
+        }
+        generateWorld(seed.toString());
+    }
+
+    /**
+     * Load a game from the stored save.
+     */
+    private void loadGame() {
+        // Todo
+    }
 
     /**
      * Method used for autograding and testing the game code. The input string will be a series
@@ -85,27 +111,36 @@ public class Game {
      * @return the 2D TETile[][] representing the state of the world
      */
     public TETile[][] playWithInputString(String input) {
-        // Fill out this method to run the game using the input passed in,
-        // and return a 2D tile representation of the world that would have been
-        // drawn if the same inputs had been given to playWithKeyboard().
+        /*
+         * Fill out this method to run the game using the input passed in,
+         * and return a 2D tile representation of the world that would have been
+         * drawn if the same inputs had been given to playWithKeyboard().
+         */
+        ter.initialize(WIDTH, HEIGHT);
         input = input.toLowerCase();
-        char mode = input.charAt(0);
 
-        // example: N3412S should generate a world with seed 3412
+        char mode = input.charAt(0);
         if (mode == 'n') {
             if (input.charAt(input.length() - 1) == 's') {
                 String seed = input.substring(1, input.length() - 1);
-                map = new Map(WIDTH, HEIGHT, Long.parseLong(seed));
-                map.generateWorld();
-
-                ter.initialize(WIDTH, HEIGHT);
-                ter.renderFrame(map.TETileMatrix());
+                generateWorld(seed);
             }
         } else if (mode == 'l') {
             return null;
+
         } else if (mode == 'q') {
-            return null;
+            System.exit(0);
         }
         return map.TETileMatrix();
+    }
+
+    /**
+     * Given a seed String, creates new map object, generates a new world, then renders the world.
+     */
+    private void generateWorld(String seed) {
+        map = new Map(WIDTH, HEIGHT, seed.hashCode());
+        map.generateWorld();
+        StdDraw.setFont(tileFont);
+        ter.renderFrame(map.TETileMatrix());
     }
 }
